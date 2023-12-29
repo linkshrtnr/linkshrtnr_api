d2f1501148023a5b7dc2e19f160d5571d7cf5faa
David Huertas <davidhuertasortiz@gmail.com>
2023-12-29 08:24:00 +0100

    feat: Add Tailwind CSS and custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./templates/*.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...fontFamily.sans],
+            },
+        },
+    },
+};
