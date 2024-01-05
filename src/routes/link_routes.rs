1494804b14a15cdba1521cd089b93e2bb95a4015
David Huertas <davidhuertasortiz@gmail.com>
2024-01-05 16:30:50 +0100

    feat: Refactor link_routes.rs and add unique constraint to short_url column

--- src/routes/link_routes.rs
+++ src/routes/link_routes.rs
@@ -3,48 +3,58 @@ use axum::{http::StatusCode, routing::post, Extension, Form, Router};
 use rand;
 use rand::Rng;
 use serde::Deserialize;
-use sqlx::{Error, PgPool};
-use tracing::info;
+use sqlx::{Error, PgPool, Row};
 
 #[derive(sqlx::FromRow, Debug, Deserialize)] // Add this line
 struct LinkRequest {
     original_url: String,
     path: String,
 }
-
+#[derive(sqlx::FromRow, Debug, Deserialize)] // Add this line
+struct LinkResponse {
+    short_url: String,
+}
 pub fn get_routes() -> Router {
     Router::new().route("/", post(add_link))
 }
 
-async fn add_link(ctx: Extension<ApiContext>, Form(payload): Form<LinkRequest>) -> StatusCode {
-    // Get the link from the form
-    info!("{:?}", payload.original_url);
-    info!("{:?}", payload.path);
-
+async fn add_link(
+    ctx: Extension<ApiContext>,
+    Form(payload): Form<LinkRequest>,
+) -> Result<String, StatusCode> {
     // Store the links in the database
-    if let Err(err) = insert_link(&payload, &ctx.db).await {
-        // Handle the error, log it, and return an appropriate status code
-        eprintln!("Error inserting link: {}", err);
-        return StatusCode::INTERNAL_SERVER_ERROR;
-    }
+    let link = match insert_link(&payload, &ctx.db).await {
+        Ok(link) => link,
+        Err(err) => {
+            eprintln!("Error inserting link: {}", err);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+    //return html
 
-    // Return the link / return text with link and show it in the UI
-    StatusCode::OK
+    Ok(format!(
+        "<div>Your link: <a href=\"http://localhost:3001/{}\" target=\"_blank\">https://lurl.es/{}</a></div>",
+        link.short_url, link.short_url
+    ))
 }
-async fn insert_link(payload: &LinkRequest, pool: &PgPool) -> Result<(), Error> {
+async fn insert_link(payload: &LinkRequest, pool: &PgPool) -> Result<LinkResponse, Error> {
     let path = if payload.path.is_empty() {
         generate_random_string(5)
     } else {
         payload.path.clone()
     };
 
-    let _insertion = sqlx::query("INSERT INTO links (original_url, short_url) VALUES ($1, $2);")
-        .bind(&payload.original_url)
-        .bind(&path)
-        .execute(pool)
-        .await?;
-
-    Ok(())
+    let insertion = sqlx::query(
+        "INSERT INTO links (original_url, short_url) VALUES ($1, $2) RETURNING short_url",
+    )
+    .bind(&payload.original_url)
+    .bind(&path)
+    .fetch_one(pool)
+    .await?;
+    let link = LinkResponse {
+        short_url: insertion.try_get("short_url")?,
+    };
+    Ok(link)
 }
 fn generate_random_string(length: usize) -> String {
     let mut rng = rand::thread_rng();
